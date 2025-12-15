7d6304f989f4f01995cb857b9aa329ab35f87e44
BerkayGithub <berkay.danis@hotmail.com>
2025-12-15 15:43:35 +0300

    dependency injection with get_it

--- lib/blocs/weather/weather_bloc.dart
+++ lib/blocs/weather/weather_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_weather_app_with_bloc/data/weather_repository.dart';
+import 'package:flutter_weather_app_with_bloc/locator.dart';
 
 import '../../models/weather.dart';
 
@@ -10,18 +11,17 @@ part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  final WeatherRepository weatherRepository;
+  final WeatherRepository weatherRepository = locator<WeatherRepository>();
 
-  WeatherBloc({required this.weatherRepository}) : super(WeatherInitial()) {
+  WeatherBloc() : super(WeatherInitial()) {
     on<FetchWeatherEvent>(_fetchWeather);
   }
-
 
   Future<void> _fetchWeather(FetchWeatherEvent event, Emitter<WeatherState> emit) async {
     emit(WeatherLoadingState());
     try{
-      await weatherRepository.fetchWeather(event.sehirAdi);
-      emit(WeatherLoadedState(weather: Weather()));
+      final Weather getirilenWeather = await weatherRepository.fetchWeather(event.sehirAdi);
+      emit(WeatherLoadedState(weather: getirilenWeather));
     }catch(e){
       emit(WeatherErrorState());
     }
